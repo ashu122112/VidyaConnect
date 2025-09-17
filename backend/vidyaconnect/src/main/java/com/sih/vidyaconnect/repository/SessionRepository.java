fa73baf5d674215c721099a6e30bc7045204a389
Agam verma <agamverma00@gmail.com>
2025-09-17 19:48:01 +0530

    made db configurations

--- backend/vidyaconnect/src/main/java/com/sih/vidyaconnect/repository/SessionRepository.java
+++ backend/vidyaconnect/src/main/java/com/sih/vidyaconnect/repository/SessionRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
     List<Session> findByActive(boolean active);
-    List<Session> findByStatus(String status);
+ //   List<Session> findByStatus(String status);
 }
